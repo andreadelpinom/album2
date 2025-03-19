6d2928dbacd8358e2a9236becd1b7dacf0720c7e
andreadelpinom <andreadelpino@uees.edu.ec>
2025-03-18 20:16:35 -0500

    se agrego fotoRango.ejs en views, y se agrego en fotos.js 2 metodos para que aparezca la vista filtrada, dependiendo a lo que ingrese el usuario en el input

--- routes/fotos.js
+++ routes/fotos.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 
-const Sequelize = require('sequelize');
+//const Sequelize = require('sequelize');
+const { Sequelize, Op } = require('sequelize');
 const Foto = require('../models').foto; 
 const Etiqueta = require('../models').etiqueta; 
 
@@ -34,6 +35,62 @@ router.get('/findAll/view', function(req, res, next) {
     .catch(error => res.status(400).send(error))
 
  
+});
+
+router.get('/findAllByRate/json', function (req, res, next) {
+    let lower = parseFloat(req.query.lower);
+    let higher = parseFloat(req.query.higher);
+
+    Foto.findAll({
+        attributes: {
+            exclude:
+                ["updatedAt"]
+        },
+        include: [{
+            model: Etiqueta,
+            attributes: ['texto'],
+            through: { attributes: [] }
+        }],
+        where: {
+            calificacion: {
+                [Op.between]: [lower, higher]
+            }
+        }
+    })
+        .then(fotos => {
+             res.json(fotos);
+        })
+        .catch(error =>
+             res.status(400).send(error))
+});
+
+// Nueva ruta para mostrar fotos filtradas por calificación en vista
+router.get('/findAllByRate/view', function (req, res, next) {
+    let lower = parseFloat(req.query.lower || 0);
+    let higher = parseFloat(req.query.higher || 5);
+
+    Foto.findAll({
+        attributes: { exclude: ["updatedAt"] },
+        include: [{
+            model: Etiqueta,
+            attributes: ['texto'],
+            through: { attributes: [] }
+        }],
+        where: {
+            calificacion: {
+                [Op.between]: [lower, higher]
+            }
+        }
+    })
+        .then(fotos => {
+            res.render('fotoRango', { 
+                title: 'Fotos por Calificación', 
+                arrFotos: fotos,
+                lower: lower,
+                higher: higher
+            });
+        })
+        .catch(error => res.status(400).send(error))
 });
 
 module.exports = router;
